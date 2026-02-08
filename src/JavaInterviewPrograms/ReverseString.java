08da392d43a17e34400453149bf35a4d6cfa0567
venky <chvsubhash417@gmail.com>
2026-02-08 10:19:19 +0530

    InitialCommit

---
+++ src/JavaInterviewPrograms/ReverseString.java
@@ -0,0 +1,17 @@
+package JavaInterviewPrograms;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str="hello";
+		String rev = "";
+		
+		for(int i=str.length()-1; i>=0; i--) {
+			 rev=rev+str.charAt(i);
+		}
+		System.out.println("the actual string is : "+ str);
+		System.out.println("The reversed string is : " + rev);
+	}
+
+}
---
+++ src/JavaInterviewPrograms/ReveseString2.java
@@ -0,0 +1,10 @@
+package JavaInterviewPrograms;
+
+public class ReveseString2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
