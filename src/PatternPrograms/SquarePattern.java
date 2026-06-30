c385700ba96c362cfeb55b214b807454302befd4
venky <chvsubhash417@gmail.com>
2026-06-30 11:10:50 +0530

    SquarePattern

--- src/PatternPrograms/SquarePattern.java
+++ src/PatternPrograms/SquarePattern.java
@@ -4,9 +4,13 @@ public class SquarePattern {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int n=4;
+		int n=4; // size of the patterns
+		
+		// outer for loop for rows
 		for(int i=0;i<n;i++) {
+			// inner for loop for columns
 			for(int j=0;j<n;j++) {
+				// prints '*' in a row and moves to next line after all iterations
 				System.out.print("* ");
 			}
 			System.out.println();
